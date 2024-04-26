dc0e1b290854b3bf5aa429c6c26f862bc719e5a5
Sujju666 <sujalbalar02@gmail.com>
2024-04-25 20:09:11 -0400

    Task 1-13 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,16 +40,19 @@ public_users.get('/', function (req, res) {
     promise
     .then(result => {
         return res.status(200).json({ result });
-    })   
+    })
+    .catch(err => {
+        return  res.status(404).json({message : "DAta not found."});
+    }) 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
 
     const isbn = req.params.isbn;
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(books[req.params.isbn])
+            resolve(books[isbn])
         }, 777);
     });
 
@@ -64,7 +67,7 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author', async function (req, res) {
+public_users.get('/author/:author', function (req, res) {
 
     const author = req.params.author;
     const promise = new Promise((resolve, reject) => {
@@ -86,7 +89,7 @@ public_users.get('/author/:author', async function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title', async function (req, res) {
+public_users.get('/title/:title', function (req, res) {
 
     const title = req.params.title;
     const promise = new Promise( (resolve, reject) => {
